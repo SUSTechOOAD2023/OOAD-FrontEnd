8df559f476c8c35ebceda85499e584b8307bfecb
sjkmost <sjkmoster@outlook.com>
2023-10-28 02:27:02 +0800

    rewrite of ooad genshin

---
+++ src/app/Copyright.tsx
@@ -0,0 +1,9 @@
+import Typography from '@mui/material/Typography';
+
+export default function Copyright(props: any) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+        Copyright ©Sending 2023.
+    </Typography>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,115 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import Link from 'next/link';
+import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Copyright from './Copyright';
 
-export default function Home() {
+const SUSTechLogoStyle = {
+  borderRadius: "50%"
+}
+
+const iconStyle = {
+  borderRadius: "5%"
+}
+
+export default function LandingPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <Container component="main">
+      <Stack alignItems="center" marginTop={12} marginBottom={12} spacing={4}>
+        <Stack direction="row" alignItems="center" spacing={2}>
+          <Image
+            src="/sustech_logo.png"
+            alt="SUSTech Logo"
+            width={100}
+            height={100}
+            style={SUSTechLogoStyle}
+            priority
+          />
+          <Typography variant="h1" fontWeight={500}>
+            Project Helper
+          </Typography>
+        </Stack>
+        <Stack direction="row" alignItems="center" spacing={3}>
+          <Link href="/login">
+            <Button variant="contained">
+              Log In
+            </Button>
+          </Link>
+          <Link href="/register">
+            <Button variant="contained">
+              Register
+            </Button>
+          </Link>
+        </Stack>
+      </Stack>
+      <Stack alignItems="center" marginTop={4} marginBottom={4} spacing={5}>
+        <Stack direction="row" marginBottom={4} spacing={10}>
+          <Stack alignItems="center">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
+              src="/data_icon.png"
+              alt="Data"
+              width={150}
+              height={150}
+              style={iconStyle}
               priority
             />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+            <Typography variant="subtitle1">
+              Data
+            </Typography>
+          </Stack>
+          <Stack alignItems="center">
+            <Image
+              src="/view_icon.png"
+              alt="View"
+              width={150}
+              height={150}
+              style={iconStyle}
+              priority
+            />
+            <Typography variant="subtitle1">
+              View
+            </Typography>
+          </Stack>
+        </Stack>
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
+          src="/paimon.png"
+          alt="Genshin Impact"
+          width={150}
+          height={150}
           priority
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <Stack direction="row" marginTop={4} spacing={10}>
+          <Stack alignItems="center">
+            <Image
+              src="/show_icon.png"
+              alt="Show"
+              width={150}
+              height={150}
+              style={iconStyle}
+              priority
+            />
+            <Typography variant="subtitle1">
+              Show
+            </Typography>
+          </Stack>
+          <Stack alignItems="center">
+            <Image
+              src="/setting_icon.png"
+              alt="Setting"
+              width={150}
+              height={150}
+              style={iconStyle}
+              priority
+            />
+            <Typography variant="subtitle1">
+              Setting
+            </Typography>
+          </Stack>
+        </Stack>
+      </Stack>
+      <Copyright marginTop={8} marginBottom={4} />
+    </Container>
   )
 }
