7c9758a97de40d46d6253ce29b0b84cefdb0b696
Kevin327 <1779894826@qq.com>
2023-12-29 14:33:30 +0800

    change password

--- src/app/dashboard/setting/changePassword/page.tsx
+++ src/app/dashboard/setting/changePassword/page.tsx
@@ -1,69 +1,117 @@
 'use client'
-import { Box, Button, TextField, Typography } from '@mui/material';
-import React, { useState } from 'react';
 
-export default function ChangePassword() {
-    const [currentPassword, setCurrentPassword] = useState('');
-    const [newPassword, setNewPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+import Alert from '@mui/material/Alert';
+import { useState } from 'react';
+import MenuItem from '@mui/material/MenuItem';
+import postChange from './passwordHandler';
 
-    const handleCurrentPasswordChange = (event) => {
-        setCurrentPassword(event.target.value);
-    };
+export default function LoginPage() {
+  const router = useRouter()
+  const [alertDisplay, setAlertDisplay] = useState("none")
+  const [alertText, setAlertText] = useState("")
 
-    const handleNewPasswordChange = (event) => {
-        setNewPassword(event.target.value);
-    };
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+        email: data.get("oldPassword"),
+        password: data.get("newPassword"),
+        identity: data.get("newPassword2"), 
+      });
+    const old = data.get("oldPassword")?.toString();
+    const new1 = data.get("newPassword")?.toString();
+    const new2 = data.get("newPassword2")?.toString();
+    if (old && new1 && new2) {
+        if (new1 !== new2){
+        setAlertText("The entered passwords do not match!")
+        setAlertDisplay("flex")
+        }
+        else {
+            await postChange({
+                "old": old,
+                "new": new2
+            })
+        }
+    }
+    else {
+        setAlertText("All fields should not be empty!")
+        setAlertDisplay("flex")
+      }
 
-    const handleConfirmPasswordChange = (event) => {
-        setConfirmPassword(event.target.value);
-    };
+  };
 
-    const handleSubmit = () => {
-        // Implement password change logic here
-        console.log("Password changed to:", newPassword);
-    };
-
-    return (
-        <Box display="flex" flexDirection="column" alignItems="center" width="60%" margin="auto">
-            <Typography variant="h4" marginTop={2}>
-                Change Password
-            </Typography>
-            <TextField
-                variant="outlined"
-                margin="normal"
-                label="Current Password"
-                type="password"
-                value={currentPassword}
-                onChange={handleCurrentPasswordChange}
-                style={{ width: '70%' }}
-            />
-            <TextField
-                variant="outlined"
-                margin="normal"
-                label="New Password"
-                type="password"
-                value={newPassword}
-                onChange={handleNewPasswordChange}
-                style={{ width: '70%' }}
-            />
-            <TextField
-                variant="outlined"
-                margin="normal"
-                label="Confirm New Password"
-                type="password"
-                value={confirmPassword}
-                onChange={handleConfirmPasswordChange}
-                style={{ width: '70%' }}
-            />
-            <Button
-                variant="contained"
-                color="primary"
-                onClick={handleSubmit}
-                style={{ marginTop: '20px' }}
-            >
-                Submit
-            </Button>
+  return (
+    <Container component="main" maxWidth="xs" sx={{ minHeight: "100vh" }}>
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Change Password
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+        <TextField
+            margin="normal"
+            required
+            fullWidth
+            label="Old Password"
+            name="oldPassword"
+            type="password"
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            label="New Password"
+            name="newPassword"
+            type="password"
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            label="Repeat New Password"
+            name="newPassword2"
+            type="password"
+          />
+          <Alert 
+            variant="outlined" 
+            severity="error" 
+            onClose={() => { setAlertDisplay("none") }}
+            sx={{
+              display: alertDisplay
+            }}>
+            {alertText}
+          </Alert>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 1, mb: 2 }}
+          >
+            Submit
+          </Button>
         </Box>
-    );
+      </Box>
+    </Container>
+  )
 }
