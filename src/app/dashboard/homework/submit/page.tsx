34f9f0f9da49c49fef342762e4515b05d76fc254
SPK327 <1779894826@qq.com>
2024-01-01 16:05:44 +0800

    fix errors not satisfy ESLint

--- src/app/dashboard/homework/submit/page.tsx
+++ src/app/dashboard/homework/submit/page.tsx
@@ -9,25 +9,29 @@ import {getId} from "@/app/dashboard/accountIdHandler";
 
 
 export default function SubmitHomework() {
-  const [dueDate, setDueDate] = useState('');
-  const [points, setPoints] = useState('');
-  const [description, setDescription] = useState('');
-  const [files, setFiles] = useState([]);
-  const handleDelete = (index) => {
-    const newFiles = [...files];
-    newFiles.splice(index, 1);
-    setFiles(newFiles);
-  };
-  const assignmentInfo = {
-    dueDate: new Date('2023-12-01T23:59:59'),
-    maxPoint: 100,
-    description: "Play Genshin for 3 hours"
-  };
+    const [dueDate, setDueDate] = useState('');
+    const [points, setPoints] = useState('');
+    const [description, setDescription] = useState('');
+    const [files, setFiles] = useState<File[]>([]);
 
-    const handleFileChange = (event) => {
-        const newFiles = Array.from(event.target.files);
-        setFiles([...files, ...newFiles]);
+    const handleDelete = (index) => {
+        const newFiles = [...files];
+        newFiles.splice(index, 1);
+        setFiles(newFiles);
     };
+    const assignmentInfo = {
+        dueDate: new Date('2023-12-01T23:59:59'),
+        maxPoint: 100,
+        description: "Play Genshin for 3 hours"
+    };
+
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files) {
+            const newFiles = Array.from(event.target.files) as File[];
+            setFiles([...files, ...newFiles]);
+        }
+    };
+
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -42,111 +46,111 @@ export default function SubmitHomework() {
         }
     };
 
-  return (
-    <Box sx={{ maxWidth: 500, margin: 'auto', mt: 5 }}>
-      <Box display="flex" alignContent="center" justifyContent="center">
-      <PublishIcon color="secondary" sx={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          border: '2px solid #000',
-          borderColor: 'purple', 
-          borderRadius: '50%',
-          width: '40px',
-          height: '40px',
-      }}/>
-      <Box width={20}></Box>
-        <Typography variant="h4" gutterBottom>
-          Submit Page
-        </Typography>
-      </Box>
-      <Grid container spacing={2}>
+    return (
+        <Box sx={{ maxWidth: 500, margin: 'auto', mt: 5 }}>
+            <Box display="flex" alignContent="center" justifyContent="center">
+                <PublishIcon color="secondary" sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    border: '2px solid #000',
+                    borderColor: 'purple',
+                    borderRadius: '50%',
+                    width: '40px',
+                    height: '40px',
+                }}/>
+                <Box width={20}></Box>
+                <Typography variant="h4" gutterBottom>
+                    Submit Page
+                </Typography>
+            </Box>
+            <Grid container spacing={2}>
 
-        <Grid item xs={6}>
-          <Card sx={{ minWidth: 275, marginBottom: '20px' }}>
+                <Grid item xs={6}>
+                    <Card sx={{ minWidth: 275, marginBottom: '20px' }}>
 
-            <CardContent>
-              <Typography variant="h5" component="div">
-                Due Date
-              </Typography>
-              <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                {/* <Typography sx={{ mb: 1.5 }} color="text.secondary"> */}
-                  {assignmentInfo.dueDate.toLocaleString()}
-                {/* </Typography> */}
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
+                        <CardContent>
+                            <Typography variant="h5" component="div">
+                                Due Date
+                            </Typography>
+                            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                {/* <Typography sx={{ mb: 1.5 }} color="text.secondary"> */}
+                                {assignmentInfo.dueDate.toLocaleString()}
+                                {/* </Typography> */}
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
 
-        <Grid item xs={1}>
-        </Grid>
+                <Grid item xs={1}>
+                </Grid>
 
-        <Grid item xs={4}>
-          <Card sx={{ minWidth: 275, marginBottom: '20px' }}>
-            <CardContent>
-              <Typography variant="h5" component="div">
-                Points Possible
-              </Typography>
-              <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                100
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-      </Grid>
-      <Typography variant="h6" gutterBottom>
-        Description
-      </Typography>
-      <Typography marginBottom="18px">
-        {assignmentInfo.description}
-      </Typography>
-      <Typography variant="h6" gutterBottom>
-        Text Submission
-      </Typography>
-      <Grid container spacing={2}>
-        <Grid item xs={8} minWidth="120%" marginLeft="-5px">
-          <TextField
-            multiline
-            minRows={5}
-            placeholder="Enter your submission here"
-            style={{ width: '100%' }}
-          // value={description}
-          />
-        </Grid>
-        <Grid item>
-        </Grid>
-      </Grid>
-        <ul>
-            {files.map((file, index) => (
-                <li key={index}>
-                    <span>{file.name} - {file.size} bytes</span>
-                    <Button onClick={() => handleDelete(index)} type="danger" size="mini">Delete</Button>
-                </li>
-            ))}
-        </ul>
-      <Box display="flex" minWidth="120%" alignContent="center" justifyContent="space-around">
-        <Button
-          variant="contained"
-          component="label"
-        // sx={{ mt: 2, mb: 2 }}
-        >
-          Upload File
-          <input
-            type="file"
-            hidden
-            onChange={handleFileChange}
-          />
-        </Button>
-        {/* <Box width={100} /> */}
-        <Button
-          variant="contained"
-          color="primary"
-          // fullWidth
-          onClick={handleSubmit}
-        >
-          Submit
-        </Button>
-      </Box>
-    </Box>
-  );
+                <Grid item xs={4}>
+                    <Card sx={{ minWidth: 275, marginBottom: '20px' }}>
+                        <CardContent>
+                            <Typography variant="h5" component="div">
+                                Points Possible
+                            </Typography>
+                            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                100
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Grid>
+            <Typography variant="h6" gutterBottom>
+                Description
+            </Typography>
+            <Typography marginBottom="18px">
+                {assignmentInfo.description}
+            </Typography>
+            <Typography variant="h6" gutterBottom>
+                Text Submission
+            </Typography>
+            <Grid container spacing={2}>
+                <Grid item xs={8} minWidth="120%" marginLeft="-5px">
+                    <TextField
+                        multiline
+                        minRows={5}
+                        placeholder="Enter your submission here"
+                        style={{ width: '100%' }}
+                        // value={description}
+                    />
+                </Grid>
+                <Grid item>
+                </Grid>
+            </Grid>
+            <ul>
+                {files.map((file, index) => (
+                    <li key={index}>
+                        <span>{file.name} - {file.size} bytes</span>
+                        <Button onClick={() => handleDelete(index)} size="small">Delete</Button>
+                    </li>
+                ))}
+            </ul>
+            <Box display="flex" minWidth="120%" alignContent="center" justifyContent="space-around">
+                <Button
+                    variant="contained"
+                    component="label"
+                    // sx={{ mt: 2, mb: 2 }}
+                >
+                    Upload File
+                    <input
+                        type="file"
+                        hidden
+                        onChange={handleFileChange}
+                    />
+                </Button>
+                {/* <Box width={100} /> */}
+                <Button
+                    variant="contained"
+                    color="primary"
+                    // fullWidth
+                    onClick={handleSubmit}
+                >
+                    Submit
+                </Button>
+            </Box>
+        </Box>
+    );
 }
